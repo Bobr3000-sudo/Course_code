75bbc78e0cca2b4838ca69d3897f2fe11fece6f3
Bobr3000-sudo <kostianty1@gmail.com>
2025-03-14 22:53:06 +0200

    New file norm

--- Project/New.cpp
+++ Project/New.cpp
@@ -8,25 +8,6 @@
 #include <ncurses.h>
 #include <curses.h>
 
-bool end_game = true;
-const int WIDTH = 15;
-const int HEIGHT = 11;
-int character_x;
-int character_y;
-
-char map[] = "###############\n"
-"#           ! ~\n"
-"#   ###########\n"
-"#   #          \n"
-"#   #######    \n"
-"#   !     #    \n"
-"#######   #    \n"
-"      #   #    \n"
-"#######   #####\n"
-"# !       !   #\n"
-"###############\n";
-char character = '@';
-
 class tricks{
 private:
     std::vector <std::string> Quastions;
@@ -92,7 +73,6 @@ public:
     motivation_strong()
     {
         Wrong_strong = {
-            {""},
             {"Are you AI?"},
             {"The probability of guessing a random 2-letter word is approximately 0.00148 or 0.148%"},
             {"The probability of guessing a random 3-letter word is approximately 0.0000568 or 0.00568%"},
@@ -107,13 +87,121 @@ public:
 
 int main()
 {
+    tricks TRICKS;
+    answears_tricks ANSWEARS_TRICKS;
+    motivation MOTIVATION;
+    motivation_strong MOTIVATION_STRONG;
+    bool end_game = true;
+    const int WIDTH = 15;
+    const int HEIGHT = 11;
+    int character_x = 12;
+    int character_y = 9;
+    int score = 0;
+
+    char map[] = "###############\n"
+    "#           ! ~\n"
+    "#   ###########\n"
+    "#   #          \n"
+    "#   #######    \n"
+    "#   !     #    \n"
+    "#######   #    \n"
+    "      #   #    \n"
+    "#######   #####\n"
+    "# !       !   #\n"
+    "###############\n";
+    char character = '@';
     initscr();
     cbreak();
     keypad(stdscr, TRUE);
     int input_key;
-    while(end_game)
+    while (end_game)
     {
-        printw(0, map);
+        for(int i = 0; i < HEIGHT; i++)
+        {
+            mvprintw(i, 0, "%s", map + (WIDTH + 1) * i);
+        }
+        mvprintw(character_y, character_x, "%c", character);
+        int check = character_y * (WIDTH + 1) + character_x;
+        refresh();
+        input_key = getch();
+        if(input_key == KEY_LEFT)
+        {
+            if(map[check - 1] == '#')
+                continue;
+            else
+                character_x--;
+        }
+        if(input_key == KEY_RIGHT)
+        {
+            if(map[check + 1] == '#')
+                continue;
+            else
+                character_x++;
+        }
+        if(input_key == KEY_UP)
+        {
+            if(map[check - 16] == '#')
+                continue;
+            else
+                character_y--;
+        }
+        if(input_key == KEY_DOWN)
+        {
+            if(map[check + 16] == '#')
+                continue;
+            else
+                character_y++;
+        }
+        if (map[check] == '!')
+        {
+            clear();
+            mvprintw(1, 1, "You found a question! Press 'ENTER' key to see it.");
+            mvprintw(2, 1, "Press 'BACKSPACE' key to go out.");
+            refresh();
+            int press_key;
+            press_key = getch();
+            if(press_key == '\n')
+            {
+                clear();
+                map[check] = ' ';
+                std::string iteration = TRICKS.get_quastions()[rand() % TRICKS.get_quastions().size()];
+                mvprintw(1, 0, "%s", iteration.c_str());
+                auto it = std::find(TRICKS.get_quastions().begin(), TRICKS.get_quastions().end(), iteration);
+                int distance = std::distance(TRICKS.get_quastions().begin(), it);
+                char input[100];
+                int try_answear = 0;
+                while (input != ANSWEARS_TRICKS.get_answears()[distance])
+                {
+                    getnstr(input, sizeof(input) - 1);
+                    if(try_answear < 5)
+                    {
+                        clear();
+                        mvprintw(1, 0, "%s", iteration.c_str());
+                        mvprintw(4, 0, "%s", MOTIVATION.get_motivation()[rand() % MOTIVATION.get_motivation().size()].c_str());
+                        printw("\nAgain: ");
+                    }
+                    else
+                    {
+                        clear();
+                        mvprintw(1, 0, "%s", iteration.c_str());
+                        mvprintw(4, 0, "%s", MOTIVATION_STRONG.get_motivation_strong()[rand() % MOTIVATION_STRONG.get_motivation_strong().size()].c_str());
+                        printw("\nAgain: ");
+                    }
+                    try_answear++;
+                }
+                ANSWEARS_TRICKS.get_answears().erase(ANSWEARS_TRICKS.get_answears().begin() + distance);
+                TRICKS.get_quastions().erase(TRICKS.get_quastions().begin() + distance);
+                score++;
+            }
+            else if(press_key == KEY_BACKSPACE)
+            {
+                continue;
+            }
+        }
+        if(score == 4)
+        {
+            end_game = false;
+        }
     }
     endwin();
     return 0;
