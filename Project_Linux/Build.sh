b86871665f9384ef5767a228f36ee31d0ac29aa8
Bobr3000-sudo <kostianty1@gmail.com>
2025-03-19 22:44:28 +0200

    folder

--- Project_Linux/Build.sh
+++
@@ -1,3 +0,0 @@
-g++ main.cpp character.cpp map.cpp end.cpp start.cpp clear.cpp -o main -lncurses
-./main
-clear
--- Project_Linux/start.h
+++
@@ -1,9 +0,0 @@
-#ifndef START_H
-#define START_H
-
-#include <ncurses.h>
-#include <string>
-
-void start(bool &start_game_end);
-
-#endif
