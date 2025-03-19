1f40fe2a994dff473adc2a95cb445aafe0ccf08a
Bobr3000-sudo <kostianty1@gmail.com>
2025-03-19 21:17:54 +0200

    add clear file, fix story, fix mistake in input age, create functions, fix BUILD.sh

---
+++ Project/Build.sh
@@ -0,0 +1,3 @@
+g++ main.cpp character.cpp map.cpp end.cpp start.cpp clear.cpp -o main -lncurses
+./main
+clear
--- Project/Built.sh
+++
@@ -1,3 +0,0 @@
-g++ main.cpp character.cpp map.cpp end.cpp start.cpp -o main -lncurses
-./main
-clear
