ebe65e5489bfb7b05ede51caf3a1a224d694e3e3
Bobr3000-sudo <kostianty1@gmail.com>
2025-04-19 20:23:19 +0300

    scrips

--- Project/Build.sh
+++ Project/Build.sh
@@ -1,3 +1,4 @@
-g++ main.cpp character.cpp map.cpp end.cpp start.cpp clear.cpp -o main -lncurses
+g++ main.cpp character.cpp map.cpp end.cpp start.cpp clear.cpp -o main -lncurses -pg
 ./main
+gprof test gmon.out > analysis.txt
 clear
